7c79b63c93fe3fe0c405c358018dcd0ac148ff17
= <=>
2025-12-29 14:14:31 +0100

    sites

--- src/assets/data/sites.ts
+++ src/assets/data/sites.ts
@@ -3,13 +3,13 @@ import { Projet } from 'src/app/core/interfaces/projet';
 export const sites: Projet[] = [
   {
     titre: 'Site 1',
-    image: 'sites/site1/1.webp',
+    image: 'sites/1/home.webp',
     titreBouton: 'Découvrir le site',
     lien: 'sites/1',
   },
   {
     titre: 'Site 2',
-    image: 'sites/site2/2.webp',
+    image: 'sites/2/landing.webp',
     titreBouton: 'Découvrir le site',
     lien: 'sites/2',
   },
